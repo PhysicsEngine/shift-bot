26429182b069850892084e24233f67790efeaf90
Yohei Yamaguchi <joker13meister@gmail.com>
2016-05-21 18:31:33 +0900

    Add testData.sql.

---
+++ db/testData.sql
@@ -0,0 +1,3 @@
+INSERT INTO teams (id, name) VALUES(1, 'nobitanian');
+INSERT INTO members (id, name, line_id, team) VALUES(1, 'gologo13', 'dummy', 1);
+INSERT INTO members (id, name, line_id, team) VALUES(2, 'Kai', 'dummy', 1);
